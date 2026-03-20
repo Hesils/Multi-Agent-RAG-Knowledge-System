0a46714b513679cdfbe7ae0ce1f763fb582cb25a
Hesils <quentindesvignes0373@gmail.com>
2026-03-20 18:47:34 +0100

    Adding little typer CLI to be user friendly

---
+++ cli/main_cli.py
@@ -0,0 +1,19 @@
+import typer
+
+from pipelines.answering_pipeline import AnsweringPipeline
+from pipelines.rag_pipeline import RagPipeline
+from utils.chromadb_client import chromadb_client
+
+app = typer.Typer()
+
+@app.command()
+def answer(query: str):
+    answering_pipeline = AnsweringPipeline()
+    response = answering_pipeline.answer(query)
+    print(response)
+
+@app.command()
+def rag_update():
+    rag_pipeline = RagPipeline()
+    collection = chromadb_client.chroma_client.get_or_create_collection(name="identity")
+    rag_pipeline.update_db_collection_content(collection, chromadb_client)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 
-from pipelines.answering_pipeline import AnsweringPipeline
-from pipelines.rag_pipeline import RagPipeline
-from utils.chromadb_client import chromadb_client
+from cli.main_cli import app
 
 
 def init():
@@ -16,13 +14,7 @@ def init():
 
 def main():
     init()
-    # rag_pipeline = RagPipeline()
-    # collection = chromadb_client.chroma_client.get_or_create_collection(name="identity")
-    # rag_pipeline.update_db_collection_content(collection, chromadb_client)
-    user_input = "Quelles librairies python a utilisé Mr Desvignes lors de son emploi à la MAIF ?"
-    answering_pipeline = AnsweringPipeline()
-    response = answering_pipeline.answer(user_input)
-    print(response)
+    app()
 
 
 
